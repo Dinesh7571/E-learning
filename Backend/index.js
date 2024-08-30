8f368c7581e604c175c7e7d6862372a1aec4fa2b
Krishna Chauhan <sandhya756507@gmail.com>
2024-08-30 11:59:51 +0530

    krishna frontend create

--- Backend/index.js
+++ Backend/index.js
@@ -45,5 +45,5 @@ app.use((err, req, res, next) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
